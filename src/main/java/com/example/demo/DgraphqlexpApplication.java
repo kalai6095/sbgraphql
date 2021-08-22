3fc32f64d1b1e498d539360e2747b6d53b0b80d4
kalaiselvan <kalai6095@gmail.com>
2021-08-22 08:21:38 +0530

    graphql

---
+++ src/main/java/com/example/demo/DgraphqlexpApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgraphqlexpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgraphqlexpApplication.class, args);
+	}
+
+}
